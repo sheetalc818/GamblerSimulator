dcb42cbc0919748382e453aeaad02efecc553570
sheetalc818 <sheetalc818@gmail.com>
2020-01-10 18:19:02 +0530

    Added : Checking for Win or Loose after Gambler make bet of

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -1,7 +1,22 @@
-#!/bin/bash +x
+#!/bin/bash -x
 echo "==WELCOME TO GAMBLER SIMULATOR=="
 
 #Declaring Constants
 declare STAKE_AMOUNT=100;
 declare BET_AMOUNT=1;
 declare STAKE_DAYS=30;
+
+#Function for betting
+function betting()
+{
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		win=$(($STAKE_AMOUNT + 1))
+		echo $win
+	else
+		loose=$(($STAKE_AMOUNT - 1))
+		echo $loose
+	fi
+}
+#Calling betting() function
+betting
