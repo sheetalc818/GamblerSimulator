496f93da90919f72535c065b269800c439fc50bb
sheetalc818 <sheetalc818@gmail.com>
2020-01-16 17:10:27 +0530

    Added : To know each month Won or Lost by how much

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -4,7 +4,7 @@ echo "==WELCOME TO GAMBLER SIMULATOR=="
 #Constants
 declare LOOSING_AMOUNT=0
 declare STAKE_AMOUNT=100
-declare STAKE_DAYS=20
+declare STAKE_DAYS=30
 declare STAKE_BET=1
 
 #Dictionary
@@ -14,38 +14,42 @@ declare -A gamblerDictionary
 lowerLimit=50
 higherLimit=150
 totalAmount=0
+win=0
+loose=0
 
 #Function for betting
 function betting()
 {
 	for (( i=1;i<=$STAKE_DAYS;i++ ))
 	do
-		stakeAmount=$STAKE_AMOUNT
+		stakeAmount=$(($totalAmount+$STAKE_AMOUNT))
 		while [ $stakeAmount -gt $LOOSING_AMOUNT ]
 		do
-			if [ $((RANDOM%2)) -eq 1 ]
+			if [ $stakeAmount -gt 0 ]
 			then
-				stakeAmount=$(($stakeAmount + $STAKE_BET))
-			else
-				stakeAmount=$(($stakeAmount - $STAKE_BET ))
+				if [ $((RANDOM%2)) -eq 1 ]
+				then
+					stakeAmount=$(($stakeAmount + $STAKE_BET))
+				else
+					stakeAmount=$(($stakeAmount - $STAKE_BET ))
+				fi
 			fi
 
 			#If Won Credit 50
 			if [ $stakeAmount -gt $higherLimit ]
       	then
-				gamblerDictionary[$i]=$(($STAKE_AMOUNT+50))
-				totalAmount=$(($totalAmount+50))
+				((win++))
+				gamblerDictionary[$i]=50
 				break
 			#If Loose Debit 50
 			elif [ $stakeAmount -lt $lowerLimit ]
 			then
-         	gamblerDictionary[$i]=$(($STAKE_AMOUNT - 50))
-				totalAmount=$(($totalAmount-50))
+				((loose++))
+         	gamblerDictionary[$i]=-50
       		break
       	fi
 		done
 	done
-	echo $totalAmount
 }
 
 #Function to calculate Upper and Lower limit
@@ -55,10 +59,18 @@ function calLowerAndUpperLimit()
 	higherLimit=$(($(($higherLimit*$STAKE_AMOUNT))/100))
 }
 
+#Function to calculate Percentage of win or Loose in a month
+function WonOrLooseInMonth()
+{
+	winningPer=$(($win*100/$STAKE_DAYS));
+	loosingPer=$(($loose*100/$STAKE_DAYS));
+	echo ${gamblerDictionary[@]}
+}
+
 #Calling functions
 calLowerAndUpperLimit
-betting $STAKE_DAYS 
-echo ${gamblerDictionary[@]}
+betting $STAKE_DAYS
+WonOrLooseInMonth
 
 
 
